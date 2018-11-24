f98a3ec96bddc7159ad6f62ab1f02c3c239215ea
changhyeon743 <changhyeon743@gmail.com>
2018-11-25 08:50:49 +0900

    i lov commit

--- myapp/routes/views.js
+++ myapp/routes/views.js
@@ -1,5 +1,7 @@
 module.exports = index;
 
+var request = require('request');
+
 function index(app) {
     app.get('/',function(req,res) {
         res.render('index',{url:"https://cdn.pixabay.com/photo/2016/11/29/04/19/beach-1867285_1280.jpg"});
@@ -9,7 +11,11 @@ function index(app) {
         res.render('search',{url:"background:url(https://cdn.pixabay.com/photo/2016/11/29/04/19/beach-1867285_1280.jpg) center"});
     })
 
-    app.get('/result',function(req,res) {
-        res.render('result',{url:"https://cdn.pixabay.com/photo/2016/11/29/04/19/beach-1867285_1280.jpg"});
+    app.get('/result/:tag',function(req,res) {
+        request(encodeURI("http://localhost:3000/hashtags/"+req.params.tag+"?count=10"), function (error, response, body) {
+            console.log('body:', JSON.stringify(body)); // Print the HTML for the Google homepage.
+            
+            res.render('result',{datas: body.data,tag:req.params.tag});
+        });
     })
 }
