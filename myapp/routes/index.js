6dc6cfd4e6ebbc9f05ffd783712f055a37d90af8
changhyeon743 <changhyeon743@gmail.com>
2018-11-25 11:23:47 +0900

    aa

--- myapp/routes/index.js
+++ myapp/routes/index.js
@@ -5,34 +5,38 @@ var request = require('request');
 
 /* GET home page. */
 function index(app) {
-  
 
-  app.get('/hashtags/:tag', function(req, res) {
-    var url = encodeURI('https://www.instagram.com/explore/tags/'+req.params.tag);
+
+  app.get('/hashtags/:tag', function (req, res) {
+    var url = encodeURI('https://www.instagram.com/explore/tags/' + req.params.tag);
     var count = 10;
     if (req.query.count) {
       count = req.query.count;
     }
     request(url, function (error, response, body) {
       //TODO: variable names
-        if(error) throw error
-        let $ = cheerio.load(body);
+      if (error) throw error
+      let $ = cheerio.load(body);
 
-        //HTML data: 가공되지 않은 데이터
-        //replace, slice 는 json 형태로 만들기 위해 필요한 코드.
-        let htmlData = $('body').children().next().html().replace('window._sharedData = ','').slice(0,-1);
-        
-        ///htmlData 를 json으로 파싱
-        let jsonData = null;
-        try {
-          jsonData = JSON.parse(htmlData);
-        } catch(err) {
-          console.error(err)
-        }
-        if (jsonData == null) {
-          res.sendStatus(404);
-        }
+      //HTML data: 가공되지 않은 데이터
+      //replace, slice 는 json 형태로 만들기 위해 필요한 코드.
+      let htmlData = $('body').children().next().html().replace('window._sharedData = ', '').slice(0, -1);
 
+      let isError = false;
+      ///htmlData 를 json으로 파싱
+      let jsonData = null;
+      try {
+        jsonData = JSON.parse(htmlData);
+      } catch (err) {
+        console.error(err);
+        isError = true;
+      }
+
+      if (jsonData==null) {
+        res.sendStatus(404);
+      }
+
+      if (!isError) {
         //이 코드는 html 파일을 읽으며 작성
         let temp_data = jsonData["entry_data"]["TagPage"][0]["graphql"]["hashtag"]["edge_hashtag_to_media"]["edges"];
         ///captions 에는 태그를 포함한 유저의 글이 모아짐.
@@ -40,12 +44,12 @@ function index(app) {
         temp_data.forEach(element => {
           let temp = element["node"]["edge_media_to_caption"]["edges"][0];
           //if ["edges"][0] isn't null ~>
-          if (temp!=null) {
+          if (temp != null) {
             captions.push(temp["node"]["text"]);
           }
         });
 
-        
+
         //captions에서 정규표현식을 이용해 해쉬태그 추출
         let hashtags = [];
         captions.forEach(element => {
@@ -55,14 +59,14 @@ function index(app) {
 
         //중복되는 요소들을 Dicitonary 형태로 합침
         var dic = {};
-        
+
         for (var i = 0; i < hashtags.length; i++) {
           var num = hashtags[i];
-          dic[num] = dic[num] ? ++dic[num]: 1;
+          dic[num] = dic[num] ? ++dic[num] : 1;
         }
 
         //나온 결과 중, #태그 (자기 자신)와 null 값은 삭제
-        delete dic["#"+req.params.tag];
+        delete dic["#" + req.params.tag];
         delete dic["null"];
 
         //예외처리
@@ -72,18 +76,18 @@ function index(app) {
         //console.log(dic);
         //최종결과
         var result = [];
-        
-        
+
+
         //count가 0이 될때까지 반복
-        while(count>0) {
+        while (count > 0) {
           //최댓값 찾기
           var max = {
-            count:0
+            count: 0
           };
-          Object.keys(dic).map(function(v) {
+          Object.keys(dic).map(function (v) {
             if (dic[v] > max.count) {
               max = {
-                name : v,
+                name: v,
                 count: dic[v]
               };
             }
@@ -100,11 +104,15 @@ function index(app) {
           count: hashtags.length,
           data: result
         });
-        
-      })
-      
+
+      }
+
+
+
+    })
+
 
   });
-  
+
 }
 
--- myapp/routes/views.js
+++ myapp/routes/views.js
@@ -2,22 +2,28 @@ module.exports = index;
 
 var request = require('request');
 
-
+function getRandomImage() {
+    var n = Math.floor(Math.random() * 6) + 1; //1~4
+    return "/images/"+n+".jpg";
+}
 function index(app) {
     app.get('/',function(req,res) {
-        res.render('index',{url:"https://cdn.pixabay.com/photo/2016/11/29/04/19/beach-1867285_1280.jpg"});
+        res.render('index',{url:getRandomImage()});
     })
 
     app.get('/search',function(req,res) {
-        res.render('search',{url:"background:url(https://cdn.pixabay.com/photo/2016/11/29/04/19/beach-1867285_1280.jpg) center"});
+        res.render('search',{url:"background:url("+getRandomImage()+" center"});
     })
 
     app.get('/result/:tag',function(req,res) {
         request(encodeURI("http://localhost:3000/hashtags/"+req.params.tag+"?count=10"), function (error, response, body) {
-            var temp = JSON.parse(body);
             //console.log('body:', temp); // Print the HTML for the Google homepage.
-            
-            res.render('result',{count: temp.count,datas: temp.data,tag:req.params.tag});
+            if(response.statusCode == 404){
+                res.render('result',{count: 0,datas: [],tag:"사용되지 않는 태그입니다."});
+            }else{
+                var temp = JSON.parse(body);
+                res.render('result',{count: temp.count,datas: temp.data,tag:req.params.tag});
+            }
         });
     })
 }
